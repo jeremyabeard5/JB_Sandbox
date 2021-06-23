ec64cb078f30d7e3f551947da6274a6052ca6f1c
jeremyab5 <jeremyab5@gmail.com>
2021-06-23 00:23:02 -0600

    random whileTest

---
+++ whileTest.py
@@ -0,0 +1,12 @@
+# test
+
+print('Well...')
+while 1:
+    print('Got anything to say asshole?')
+    response = input()
+    if (response == 'no') or (response == 'No'):
+        print('Fine.')
+        break
+    else:
+        print('Really? ' + response + '??? Grow up. Try again.')
+        
