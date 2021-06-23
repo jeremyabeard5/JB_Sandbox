a895801ca9dbfa88e0d0f212738039bfffb7a015
jeremyab5 <jeremyab5@gmail.com>
2021-06-22 23:01:24 -0600

    made ageTest.py

---
+++ ageTest.py
@@ -0,0 +1,11 @@
+# This program says hello and asks for my name and age
+
+print('Hello world!')
+print('What is your name?') # ask4name
+myName = input()
+print('It is good to meet you ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?')
+myAge = input()
+print('Dang, ' + str(int(myAge)) + ' years old? You are fucking old...')
