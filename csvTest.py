0a04d9998496f56ab8ab5d83666d5aefff1ce7e6
jeremyab5 <jeremyab5@gmail.com>
2021-07-23 22:45:59 -0600

    done with draft of webbings

--- csvTest.py
+++ csvTest.py
@@ -93,7 +93,10 @@ OG_StrIWeight = []
 OG_StrIWeightICost = []
 OG_StrICost = []
 OG_IWeightICost = []
+OG_Colors = []
 
+#this for loop does 2 things at once: computes the various
+#desired ratios, AND chooses colors based on the webbing
 numRows = len(OG_Manufacturers)
 for i in range(numRows):
 #    print(i)
@@ -101,27 +104,40 @@ for i in range(numRows):
     OG_StrIWeightICost = OG_StrIWeightICost + [OG_MBS[i]/OG_Weight[i]/OG_Cost[i]]
     OG_StrICost = OG_StrICost + [OG_MBS[i]/OG_Cost[i]]
     OG_IWeightICost = OG_IWeightICost + [1/OG_Weight[i]/OG_Cost[i]]
+    if OG_Materials[i] == ('Nylon' or 'nylon'):
+        OG_Colors = OG_Colors + ['b']
+    elif OG_Materials[i] == ('Polyester' or 'polyester'):
+        OG_Colors = OG_Colors + ['g']
+    elif OG_Materials[i] == ('Dyneema' or 'dyneema'):
+        OG_Colors = OG_Colors + ['r']
+    else:
+        OG_Colors = OG_Colors + ['c']
 
 #print(Webbings, StrIWeight)
 #print(StrIWeightICost)
 #print(StrICost)
 
 
-# sortSpec sorts a given list and another list along with it
-def sortSpec(globSpec, globWebs):
+# sortSpec sorts a given list and 2 other lists along with it
+# then it prints the sorted list
+def sortSpec(globSpec, globWebs, globCols):
     spec = globSpec
     webs = globWebs
+    cols = globCols
     i=1
     while i < len(webs):
         j = 1
         while j < len(webs):
-            if spec[j] > spec[j-1]:
+            if spec[j] < spec[j-1]:
                 tmp = spec[j]
                 spec[j] = spec[j-1]
                 spec[j-1] = tmp
                 tmp = webs[j]
                 webs[j] = webs[j-1]
                 webs[j-1] = tmp
+                tmp = cols[j]
+                cols[j] = cols[j-1]
+                cols[j-1] = tmp
             j+=1
         i+=1
     i = 0
@@ -156,120 +172,155 @@ import copy
 print("\nCost")
 sortedCost = copy.copy(OG_Cost)
 webbings01 = copy.copy(spacedOGWebbings)
-sortSpec(sortedCost, webbings01)    
+colors01 = copy.copy(OG_Colors)
+sortSpec(sortedCost, webbings01, colors01)    
 
 print("\nMBS")
 sortedMBS = copy.copy(OG_MBS)
 webbings02 = copy.copy(spacedOGWebbings)
-sortSpec(sortedMBS, webbings02)
+colors02 = copy.copy(OG_Colors)
+sortSpec(sortedMBS, webbings02, colors02)
 
 print("\nWeight")
 sortedWeight = copy.copy(OG_Weight)
 webbings03 = copy.copy(spacedOGWebbings)
-sortSpec(sortedWeight, webbings03)
+colors03 = copy.copy(OG_Colors)
+sortSpec(sortedWeight, webbings03, colors03)
 
 print("\nStr / Weight")
 sortedStrIWeight = copy.copy(OG_StrIWeight)
 webbings04 = copy.copy(spacedOGWebbings)
-sortSpec(sortedStrIWeight, webbings04)
+colors04 = copy.copy(OG_Colors)
+sortSpec(sortedStrIWeight, webbings04, colors04)
 
 print("\nStr / Weight / Cost")
 sortedStrIWeightICost = copy.copy(OG_StrIWeightICost)
 webbings05 = copy.copy(spacedOGWebbings)
-sortSpec(sortedStrIWeightICost, webbings05)
+colors05 = copy.copy(OG_Colors)
+sortSpec(sortedStrIWeightICost, webbings05, colors05)
 
 print("\nStr / Cost")
 sortedStrICost = copy.copy(OG_StrICost)
 webbings06 = copy.copy(spacedOGWebbings)
-sortSpec(sortedStrICost, webbings06)
+colors06 = copy.copy(OG_Colors)
+sortSpec(sortedStrICost, webbings06, colors06)
 
 print("\n1 / Weight / Cost")
 sortedIWeightICost = copy.copy(OG_IWeightICost)
 webbings07 = copy.copy(spacedOGWebbings)
-sortSpec(sortedIWeightICost, webbings07)
+colors07 = copy.copy(OG_Colors)
+sortSpec(sortedIWeightICost, webbings07, colors07)
 
 print("\nStretch at 6kN")
 sortedStretch6kN = copy.copy(OG_Stretch6kN)
 webbings08 = copy.copy(spacedOGWebbings)
-sortSpec(sortedStretch6kN, webbings08)
+colors08 = copy.copy(OG_Colors)
+sortSpec(sortedStretch6kN, webbings08, colors08)
 
-x1 = []
+print("\nStretch at 10kN")
+sortedStretch10kN = copy.copy(OG_Stretch10kN)
+webbings09 = copy.copy(spacedOGWebbings)
+colors09 = copy.copy(OG_Colors)
+sortSpec(sortedStretch10kN, webbings09, colors09)
+
+y1 = []
 for i in range(len(OG_Webbings)):
-    x1 = x1 + [i]
+    y1 = y1 + [i]
 #xTicks = x
+    
 fig1 = plt.figure()
+fig1.suptitle('Webbing Comparison')
+plt.rcParams['axes.axisbelow'] = True
+#The following are constants that can be adjusted
+#to make the margins better
+yAxFontSz = 5 #x tick font size
+lMarg = 0.11 #left margin space
+rMarg = 0.9 #right margin space
+bMarg = 0.2 #bottom margin space
+tMarg = 0.8 #top margin space
+h_Space = 0.22 #space between plots
+w_Space = 0.5 #space between plots
+dotSz = 6
+
 sp1 = fig1.add_subplot(2,4,1)
 plt.grid(True)
-xTicks = webbings01
-plt.title('Cost')
-plt.xticks(x1, xTicks)
-plt.xticks(range(len(x1)), xTicks, rotation=90)
-plt.plot(webbings01, sortedCost, '*')
-plt.subplots_adjust(left=0.11, bottom=0.27, right=0.9, top=0.88, hspace=0.52, wspace=0.2)
+yTicks = webbings01
+plt.title('Cost (usd/m)')
+plt.yticks(y1, yTicks)
+plt.yticks(range(len(y1)), yTicks, fontsize=yAxFontSz)#, rotation=90)
+plt.scatter(sortedCost, webbings01, color = colors01, marker='o', s=dotSz)
+plt.xlim(left=0)
+plt.subplots_adjust(left=lMarg, bottom=bMarg, right=rMarg, top=tMarg, hspace=h_Space, wspace=w_Space)
 
 sp2 = fig1.add_subplot(2,4,2)
 plt.grid(True)
-xTicks = webbings02
-plt.title('MBS')
-plt.xticks(x1, xTicks)
-plt.xticks(range(len(x1)), xTicks, rotation=90)
-plt.plot(webbings02, sortedMBS, '*')
-plt.subplots_adjust(left=0.11, bottom=0.27, right=0.9, top=0.88, hspace=0.52, wspace=0.2)
-
+yTicks = webbings02
+plt.title('MBS (kN)')
+plt.yticks(y1, yTicks)
+plt.yticks(range(len(y1)), yTicks, fontsize=yAxFontSz)#, rotation=90)
+plt.scatter(sortedMBS, webbings02, color = colors02, marker='o', s=dotSz)
+plt.xlim(left=0)
+plt.subplots_adjust(left=lMarg, bottom=bMarg, right=rMarg, top=tMarg, hspace=h_Space, wspace=w_Space)
 
 sp3 = fig1.add_subplot(2,4,3)
 plt.grid(True)
-xTicks = webbings03
-plt.title('g/m')
-plt.xticks(x1, xTicks)
-plt.xticks(range(len(x1)), xTicks, rotation=90)
-plt.plot(webbings03, sortedWeight, '*')
-plt.subplots_adjust(left=0.11, bottom=0.27, right=0.9, top=0.88, hspace=0.52, wspace=0.2)
+yTicks = webbings03
+plt.title('Mass (g/m)')
+plt.yticks(y1, yTicks)
+plt.yticks(range(len(y1)), yTicks, fontsize=yAxFontSz)#, rotation=90)
+plt.scatter(sortedWeight, webbings03, color = colors03, marker='o', s=dotSz)
+plt.xlim(left=0)
+plt.subplots_adjust(left=lMarg, bottom=bMarg, right=rMarg, top=tMarg, hspace=h_Space, wspace=w_Space)
 
 sp4 = fig1.add_subplot(2,4,8)
 plt.grid(True)
-xTicks = webbings04
+yTicks = webbings04
 plt.title('MBS/Wt')
-plt.xticks(x1, xTicks)
-plt.xticks(range(len(x1)), xTicks, rotation=90)
-plt.plot(webbings04, sortedStrIWeight, '*')
-plt.subplots_adjust(left=0.11, bottom=0.27, right=0.9, top=0.88, hspace=0.52, wspace=0.2)
+plt.yticks(y1, yTicks)
+plt.yticks(range(len(y1)), yTicks, fontsize=yAxFontSz)#, rotation=90)
+plt.scatter(sortedStrIWeight, webbings04, color = colors04, marker='o', s=dotSz)
+plt.xlim(left=0)
+plt.subplots_adjust(left=lMarg, bottom=bMarg, right=rMarg, top=tMarg, hspace=h_Space, wspace=w_Space)
 
 sp5 = fig1.add_subplot(2,4,5)
 plt.grid(True)
-xTicks = webbings05
+yTicks = webbings05
 plt.title('MBS/Wt/$')
-plt.xticks(x1, xTicks)
-plt.xticks(range(len(x1)), xTicks, rotation=90)
-plt.plot(webbings05, sortedStrIWeightICost, '*')
-plt.subplots_adjust(left=0.11, bottom=0.27, right=0.9, top=0.88, hspace=0.52, wspace=0.2)
+plt.yticks(y1, yTicks)
+plt.yticks(range(len(y1)), yTicks, fontsize=yAxFontSz)#, rotation=90)
+plt.scatter(sortedStrIWeightICost, webbings05, color = colors05, marker='o', s=dotSz)
+plt.xlim(left=0)
+plt.subplots_adjust(left=lMarg, bottom=bMarg, right=rMarg, top=tMarg, hspace=h_Space, wspace=w_Space)
 
 sp6 = fig1.add_subplot(2,4,6)
 plt.grid(True)
-xTicks = webbings06
+yTicks = webbings06
 plt.title('MBS/$')
-plt.xticks(x1, xTicks)
-plt.xticks(range(len(x1)), xTicks, rotation=90)
-plt.plot(webbings06, sortedStrICost, '*')
-plt.subplots_adjust(left=0.11, bottom=0.27, right=0.9, top=0.88, hspace=0.52, wspace=0.2)
+plt.yticks(y1, yTicks)
+plt.yticks(range(len(y1)), yTicks, fontsize=yAxFontSz)#, rotation=90)
+plt.scatter(sortedStrICost, webbings06, color = colors06, marker='o', s=dotSz)
+plt.xlim(left=0)
+plt.subplots_adjust(left=lMarg, bottom=bMarg, right=rMarg, top=tMarg, hspace=h_Space, wspace=w_Space)
 
 sp7 = fig1.add_subplot(2,4,7)
 plt.grid(True)
-xTicks = webbings07
+yTicks = webbings07
 plt.title('1/Wt/$')
-plt.xticks(x1, xTicks)
-plt.xticks(range(len(x1)), xTicks, rotation=90)
-plt.plot(webbings07, sortedIWeightICost, '*')
-plt.subplots_adjust(left=0.11, bottom=0.27, right=0.9, top=0.88, hspace=0.52, wspace=0.2)
+plt.yticks(y1, yTicks)
+plt.yticks(range(len(y1)), yTicks, fontsize=yAxFontSz)#, rotation=90)
+plt.scatter(sortedIWeightICost, webbings07, color = colors07, marker='o', s=dotSz)
+plt.xlim(left=0)
+plt.subplots_adjust(left=lMarg, bottom=bMarg, right=rMarg, top=tMarg, hspace=h_Space, wspace=w_Space)
 
 sp8 = fig1.add_subplot(2,4,4)
 plt.grid(True)
-xTicks = webbings08
-plt.title('Stretch at 6kN')
-plt.xticks(x1, xTicks)
-plt.xticks(range(len(x1)), xTicks, rotation=90)
-plt.plot(webbings08, sortedStretch6kN, '*')
-plt.subplots_adjust(left=0.11, bottom=0.27, right=0.9, top=0.88, hspace=0.52, wspace=0.2)
+yTicks = webbings09
+plt.title('Stretch at 10kN (%)')
+plt.yticks(y1, yTicks)
+plt.yticks(range(len(y1)), yTicks, fontsize=yAxFontSz)#, rotation=90)
+plt.scatter(sortedStretch10kN, webbings09, color = colors09, marker='o', s=dotSz)
+plt.xlim(left=0)
+plt.subplots_adjust(left=lMarg, bottom=bMarg, right=rMarg, top=tMarg, hspace=h_Space, wspace=w_Space)
 
 
 
