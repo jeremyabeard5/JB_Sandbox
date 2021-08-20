27bb2fb89065a2b236e022161f3c099a7f576fe9
jeremyab5 <jeremyab5@gmail.com>
2021-08-20 12:44:26 -0600

    created speedtesting

---
+++ speedtesting.py
@@ -0,0 +1,15 @@
+import timeit
+
+
+
+print("-".join(str(n) for n in range(100)))
+print("-".join([str(n) for n in range(100)]))
+print("-".join(map(str, range(100))))
+
+a = timeit.timeit("-".join(str(n) for n in range(100)), number=10000)
+b = timeit.timeit("-".join([str(n) for n in range(100)]), number=10000)
+c = timeit.timeit("-".join(map(str, range(100))), number=10000)
+
+print(a)
+print(b)
+print(c)
