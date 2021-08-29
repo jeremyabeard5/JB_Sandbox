88ad2e347156ab5ef29dc8b3c4fdcde4c1c7e1b9
jeremyab5 <jeremyab5@gmail.com>
2021-08-28 23:31:39 -0600

    added bday simulation and bitmap image

---
+++ bitmapTest.py
@@ -0,0 +1,70 @@
+#! /usr/bin/python3
+# 20210828 bitmap project
+# Author: Jeremy Beard
+# Source: The Big Book of Small Python Projects
+
+import sys
+
+# (!) Try changing this multiline string to any image you like:
+
+# there are 68 periods along the top and bottom of this string:
+# (You can also copy and paste this string from
+# https://inventwithpython.com/bitmapworld.txt)
+bitmap = """
+....................................................................
+   **************   *  *** **  *      ******************************
+  ********************* ** ** *  * ****************************** *
+ **      *****************       ******************************
+          *************          **  * **** ** ************** *
+           *********            *******   **************** * *
+            ********           ***************************  *
+   *        * **** ***         *************** ******  ** *
+               ****  *         ***************   *** ***  *
+                 ******         *************    **   **  *
+                 ********        *************    *  ** ***
+                   ********         ********          * *** ****
+                   *********         ******  *        **** ** * **
+                   *********         ****** * *           *** *   *
+                     ******          ***** **             *****   *
+                     *****            **** *            ********
+                    *****             ****              *********
+                    ****              **                 *******   *
+                    ***                                       *    *
+                    **     *                    *
+...................................................................."""
+
+print('Bitmap Message, by Al Swigart al@inventwithpython.com')
+print('Enter the message to displaly with the bitmap.')
+message = input ('> ')
+if message == '':
+    sys.exit()
+    
+# loops over each line in the bitmap
+for line in bitmap.splitlines():
+    # loop over each character in the line:
+    for i, bit in enumerate(line):
+        if bit == ' ':
+            # print an empty space since there's a space in the bitmap:
+            print(' ', end='')
+        else:
+            # print a character from the message:
+            print(message[i % len(message)], end='')
+    print() #print a newline
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
